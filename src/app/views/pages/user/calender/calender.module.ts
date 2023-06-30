7986332e1dd4f3f8dd7863e4c92e152c4adf5c83
kashif <admin@kashifali.me>
2023-06-30 12:23:05 +0500

    Removed shared calender

--- src/app/views/pages/user/calender/calender.module.ts
+++ src/app/views/pages/user/calender/calender.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ViewUserCalenderComponent } from './view-user-calender/view-user-calender.component';
 import { AddUserCalenderComponent } from './add-user-calender/add-user-calender.component';
 import {RouterModule, Routes} from "@angular/router";
-import {SharedCalendarModule} from "../../../shared/calender/shared-calendar.module";
 import {FullCalendarModule} from "@fullcalendar/angular";
 
 
@@ -22,7 +21,6 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    SharedCalendarModule,
     FullCalendarModule
   ]
 })
